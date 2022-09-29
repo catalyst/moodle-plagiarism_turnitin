23d0b59a9f83e3a97db4cb7d1e11be02f5915b5c
Qihui Chan <qihuichan@catalyst-au.net>
2022-09-29 14:58:03 +1000

    Add check for coderunner questions

--- lib.php
+++ lib.php
@@ -2611,7 +2611,7 @@ class plagiarism_plugin_turnitin extends plagiarism_plugin {
             
             foreach ($attempt->get_slots() as $slot) {
                 $qa = $attempt->get_question_attempt($slot);
-                if ($qa->get_question()->get_type_name() != 'essay') {
+                if ($qa->get_question()->get_type_name() != 'essay' && $qa->get_question()->get_type_name() != 'coderunner') {
                     continue;
                 }
                 $eventdata['other']['content'] = $qa->get_response_summary();
